3b769dc9093b085c8e358c71d8a606def5fda8d4
dolphin2410 <dolphin2410@outlook.kr>
2025-09-15 14:41:35 +0900

    :art: Better advertisement styling

--- src/components/ui/KakaoAD.tsx
+++ src/components/ui/KakaoAD.tsx
@@ -1,6 +1,6 @@
 function KakaoAD() {
     return (
-        <>
+        <div className="ad-container">
             <ins className="kakao_ad_area" style={{ display: "none" }}
             data-ad-unit = "DAN-rUUUDtlwQxC289kQ"
             data-ad-width = "300"
@@ -17,7 +17,7 @@ function KakaoAD() {
             data-ad-unit = "DAN-vlR1oVCYwaQ3rZ5i"
             data-ad-width = "300"
             data-ad-height = "250"></ins>
-        </>
+        </div>
     )
 }
 
